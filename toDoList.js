cde93b125b612e93e834a4bb53122aff0c063681
HidayaAbuMteer <hidayaabumteer@gmail.com>
2025-10-03 19:04:18 +0300

    prject2edit

--- toDoList.js
+++ toDoList.js
@@ -16,14 +16,17 @@ addTaskButton.addEventListener('click', function() {
         let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
         tasks.push(newTasks);
         localStorage.setItem("tasks", JSON.stringify(tasks));
+
         const listItem = document.createElement('div');
         listItem.className = 'task-item';
         listItem.innerHTML= taskText;
         taskContainer.appendChild(listItem);
+
         let deleteButton = document.createElement('button');
         deleteButton.textContent = 'Delete';
         deleteButton.className = 'deleteButton';
         listItem.appendChild(deleteButton);
+
         deleteButton.addEventListener('click', function() {
             taskContainer.removeChild(listItem);
             let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
@@ -33,16 +36,21 @@ addTaskButton.addEventListener('click', function() {
         });
         taskInput.value = '';
         taskInput.focus();
+
         let span = document.createElement('span');
-        span.className = 'checkmark';
+        span.className = 'checkMark';
         listItem.prepend(span);
+
         span.addEventListener('click', function() {
             newTasks.completed = !newTasks.completed;
             if (newTasks.completed) {
                 listItem.classList.add('completed');
+                    span.classList.add('checked');      
             } else {
                 listItem.classList.remove('completed');
+                span.classList.remove('checked');
             }
+
             let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
             const task = tasks.find(t => t.text === taskText);
             if (task) {
