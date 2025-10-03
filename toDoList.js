5e5cd26308692a38f8a81c57ce9accbfbbc9a979
HidayaAbuMteer <hidayaabumteer@gmail.com>
2025-10-03 18:04:47 +0300

    prject2

---
+++ toDoList.js
@@ -0,0 +1,56 @@
+const taskInput = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('addTaskButton');
+const taskContainer = document.getElementById('task-container');
+
+
+addTaskButton.addEventListener('click', function() {
+    const taskText = taskInput.value.trim();
+    const newTasks={
+        text: taskInput.value,
+        completed: false,
+    }
+
+    if (taskText === '') {
+    alert('Please enter a task.');
+    }else{
+        let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+        tasks.push(newTasks);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        const listItem = document.createElement('div');
+        listItem.className = 'task-item';
+        listItem.innerHTML= taskText;
+        taskContainer.appendChild(listItem);
+        let deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.className = 'deleteButton';
+        listItem.appendChild(deleteButton);
+        deleteButton.addEventListener('click', function() {
+            taskContainer.removeChild(listItem);
+            let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+            tasks = tasks.filter(t => t.text !== taskText);
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+
+        });
+        taskInput.value = '';
+        taskInput.focus();
+        let span = document.createElement('span');
+        span.className = 'checkmark';
+        listItem.prepend(span);
+        span.addEventListener('click', function() {
+            newTasks.completed = !newTasks.completed;
+            if (newTasks.completed) {
+                listItem.classList.add('completed');
+            } else {
+                listItem.classList.remove('completed');
+            }
+            let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+            const task = tasks.find(t => t.text === taskText);
+            if (task) {
+                task.completed = newTasks.completed;
+                localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    }}
+    
+    )};
+});
+
